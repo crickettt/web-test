a66650b1a16260fa2220b7ec0065dd71c48f8011
Yahzz <96367504+crickettt@users.noreply.github.com>
2024-07-25 11:26:47 +0700

    Delete node.js

--- node.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const likeBtn = document.querySelector('.like-btn');
-    const likeCount = document.querySelector('.like-count');
-
-    likeBtn.addEventListener('click', function () {
-        let count = parseInt(likeCount.textContent);
-        likeCount.textContent = count + 1;
-    });
-});
