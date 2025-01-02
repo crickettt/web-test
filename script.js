7d92f203d4d3d0c26a02e11773b18c1b2f74e895
Yahzz Σ <96367504+crickettt@users.noreply.github.com>
2025-01-02 20:11:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// Memaksa audio diputar otomatis
 window.onload = function() {
   var audio = document.getElementById('audio');
-  audio.play().catch(function(error) {
-    console.log('Autoplay gagal, mencoba untuk memutar audio secara manual: ', error);
-    audio.muted = false; // Pastikan audio tidak dimute jika autoplay gagal
-    audio.play();
+  var image = document.getElementById('image');
+
+  // Tambahkan event listener untuk interaksi pengguna (misalnya klik gambar)
+  image.addEventListener('click', function() {
+    audio.play().catch(function(error) {
+      console.log('Autoplay gagal, mencoba untuk memutar audio secara manual: ', error);
+      audio.muted = false; // Pastikan audio tidak dimute jika autoplay gagal
+      audio.play();
+    });
   });
 };
